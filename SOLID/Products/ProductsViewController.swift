4659ac8b67a57ef6218322241fa21ca5d9cb0685
Corevent Mobile <contato.corevent@gmail.com>
2021-05-09 21:45:41 -0300

    setup tableView

--- SOLID/Products/ProductsViewController.swift
+++ SOLID/Products/ProductsViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+class ProductsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Products"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "row \(indexPath.row)"
+        return cell
     }
 
 }
